ec4a3e0d2af6fe401e405b7cbad766dfee1e1237
avsprashant <40791659+avsprashant@users.noreply.github.com>
2022-05-17 19:33:20 +0100

    Add device_name as optional param
    Add device_name as optional param in customer_gateways object

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_customer_gateway" "this" {
 
   bgp_asn    = each.value["bgp_asn"]
   ip_address = each.value["ip_address"]
+  device_name = lookup(each.value, "device_name", null)
   type       = "ipsec.1"
 
   tags = merge(

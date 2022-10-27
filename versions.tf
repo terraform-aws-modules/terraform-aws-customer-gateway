040bd6a1b5fb177fbe427e15f323196537e5ea1d
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-27 14:52:38 -0400

    feat!: Bump Terraform to v1.0+ and AWS provider to v4.0+

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.0"
 
   required_providers {
-    aws = ">= 2.23"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
   }
 }

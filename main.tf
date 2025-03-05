2f8bb07c8d432b3f5a868901b69c2d895de4c3ca
Artem Kajalainen <artem@kayalaynen.ru>
2025-03-05 02:47:39 +0200

    feat: Adjust optional params for the aws_customer_gateway, add device_name and certificate_arn (#23)
    * feat: add optional device_name parameter for the aws_customer_gateway
    
    * feat: add certificate_arn and make other params optional as well
    
    * feat: add validation for customer_gateways
    
    * Update variables.tf
    
    ---------
    
    Co-authored-by: Anton Babenko <anton@antonbabenko.com>

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 resource "aws_customer_gateway" "this" {
   for_each = var.create ? var.customer_gateways : {}
 
-  bgp_asn    = each.value["bgp_asn"]
-  ip_address = each.value["ip_address"]
-  type       = "ipsec.1"
+  bgp_asn         = try(each.value["bgp_asn"], null)
+  certificate_arn = try(each.value["certificate_arn"], null)
+  ip_address      = try(each.value["ip_address"], null)
+  device_name     = try(each.value["device_name"], null)
+  type            = "ipsec.1"
 
   tags = merge(
     {

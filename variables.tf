814b41975795dce7f88fa6dcdd8e4a028534031e
Anton Babenko <anton@antonbabenko.com>
2025-03-04 16:44:42 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,15 +14,6 @@ variable "customer_gateways" {
   description = "Maps of Customer Gateway's attributes (BGP ASN and Gateway's Internet-routable external IP address)"
   type        = map(map(any))
   default     = {}
-
-  validation {
-    condition = alltrue([
-      for gateway in var.customer_gateways : (
-        can(gateway.ip_address) || can(gateway.certificate_arn)
-      )
-    ])
-    error_message = "Each customer gateway entry must contain either 'ip_address' or 'certificate_arn'."
-  }
 }
 
 variable "tags" {
